0b20b96aedfb448bfcc03ce581fdbf803a0a3f4e
renewablezip01 <renewablezip01@gmail.com>
2025-10-06 09:18:35 +0200

    Handle database connection information through configuration file

--- src/main/java/com/webshop/dao/DatabaseConnection.java
+++ src/main/java/com/webshop/dao/DatabaseConnection.java
@@ -3,13 +3,30 @@ package com.webshop.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/webshop";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "password";
+
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
 
     static {
+        Properties props = new Properties();
+        try (InputStream input = DatabaseConnection.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new RuntimeException("Unable to find config.properties");
+            }
+            props.load(input);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load database configuration", e);
+        }
+
+        URL = props.getProperty("db.url");
+        USERNAME = props.getProperty("db.user");
+        PASSWORD = props.getProperty("db.password");
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -31,3 +48,4 @@ public class DatabaseConnection {
         }
     }
 }
+
